3c1ca31af271a89546b239f682b1070a8cb3dd91
wuqiang1985 <qqlxx@163.com>
2014-02-21 15:43:58 +0800

    modify readme.md & add project & demo

---
+++ sprite/sprite/sprite.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class sprite
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(sprite));
+            this.btnGo = new System.Windows.Forms.Button();
+            this.fbdDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.rbtnVertical = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtnHorizontal = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.llMail = new System.Windows.Forms.LinkLabel();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(290, 69);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.Location = new System.Drawing.Point(290, 23);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowser.TabIndex = 1;
+            this.btnBrowser.Text = "Browser...";
+            this.btnBrowser.UseVisualStyleBackColor = true;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 25);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(260, 21);
+            this.txtPath.TabIndex = 2;
+            // 
+            // rbtnVertical
+            // 
+            this.rbtnVertical.AutoSize = true;
+            this.rbtnVertical.Checked = true;
+            this.rbtnVertical.Location = new System.Drawing.Point(6, 20);
+            this.rbtnVertical.Name = "rbtnVertical";
+            this.rbtnVertical.Size = new System.Drawing.Size(71, 16);
+            this.rbtnVertical.TabIndex = 4;
+            this.rbtnVertical.TabStop = true;
+            this.rbtnVertical.Text = "Vertical";
+            this.rbtnVertical.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbtnHorizontal);
+            this.groupBox1.Controls.Add(this.rbtnVertical);
+            this.groupBox1.Location = new System.Drawing.Point(12, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(176, 42);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sprite order";
+            // 
+            // rbtnHorizontal
+            // 
+            this.rbtnHorizontal.AutoSize = true;
+            this.rbtnHorizontal.Location = new System.Drawing.Point(83, 20);
+            this.rbtnHorizontal.Name = "rbtnHorizontal";
+            this.rbtnHorizontal.Size = new System.Drawing.Size(83, 16);
+            this.rbtnHorizontal.TabIndex = 5;
+            this.rbtnHorizontal.Text = "Horizontal";
+            this.rbtnHorizontal.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(268, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "© 2014 by";
+            // 
+            // llMail
+            // 
+            this.llMail.AutoSize = true;
+            this.llMail.Location = new System.Drawing.Point(329, 158);
+            this.llMail.Name = "llMail";
+            this.llMail.Size = new System.Drawing.Size(47, 12);
+            this.llMail.TabIndex = 7;
+            this.llMail.TabStop = true;
+            this.llMail.Text = "wuqiang";
+            this.llMail.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llMail_LinkClicked);
+            // 
+            // sprite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 179);
+            this.Controls.Add(this.llMail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnBrowser);
+            this.Controls.Add(this.btnGo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "sprite";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sprite";
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.sprite_HelpButtonClicked);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.FolderBrowserDialog fbdDialog;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.RadioButton rbtnVertical;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbtnHorizontal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel llMail;
+    }
+}
+
---
+++ sprite/sprite/sprite.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace WindowsFormsApplication1
+{
+    public enum SpriteType
+    {
+        Vertical,
+        Horizontal
+    }
+
+    public partial class sprite : Form
+    {
+
+        const string HTML_TEMPLATE = @"
+<!doctype html>
+<html lang=""en"">
+<head>
+	<meta charset=""UTF-8"">
+	<title>Sprite Test</title>
+	<style>
+		.sprite {{ background: url(sprite.png); display: block; }}
+		{0}
+	</style>
+</head>
+<body>
+	{1}
+</body>
+</html>";
+        const string IMAGE_STILE_V = "\t\t{0} {{ width: {1}px; height: {2}px; background-position: 0 {3}px; }}{4}";
+        const string IMAGE_STILE_H = "\t\t{0} {{ width: {1}px; height: {2}px; background-position: {3}px 0; }}{4}";
+        const string IMAGE_CONTAINER = "\t<a class=\"sprite {0}\"></a>{1}";
+
+        static List<string> pseudoClassList;
+
+        public sprite()
+        {
+            InitializeComponent();
+
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            pseudoClassList = new List<string>();
+            pseudoClassList.Add("link");
+            pseudoClassList.Add("visited");
+            pseudoClassList.Add("hover");
+            pseudoClassList.Add("active");
+            pseudoClassList.Add("focus");
+            pseudoClassList.Add("before");
+            pseudoClassList.Add("after");
+            pseudoClassList.Add("lang");
+
+            #region name rule test
+            /*
+            string s = GetClassName("wechat"); // wechat
+            string s1 = GetClassName("wechat_hover"); // wechat:hover
+            string s2 = GetClassName("wechat-hover"); // wechat:hover
+            string s3 = GetClassName("ibg_wechat_hover"); // ibg_wechat:hover
+            string s4 = GetClassName("ibg-wechat-hover"); // ibg-wechat:hover
+
+            string s5 = GetClassName("wechat.ibg"); // .wechat .ibg
+            string s6 = GetClassName("wechat.ibg.wechat_focus"); // .wechat .ibg .wechat:focus
+            string s7 = GetClassName("wechat.ibg.wechat_none"); // .wechat .ibg . wechat_none
+            string s8 = GetClassName("wechat_hover.wechat_none"); // .wechat:hover .wechat_none
+            string s9 = GetClassName("wechat-hover.wechat-none"); // .wechat:hover .wechat-none
+            */
+            #endregion
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            string path = txtPath.Text.Trim();
+
+            if (!string.IsNullOrEmpty(path))
+            {
+                string[] images = Directory.GetFiles(path, "*.png", SearchOption.AllDirectories);
+
+                SpriteType type = rbtnVertical.Checked ? SpriteType.Vertical : SpriteType.Horizontal;
+
+                Bitmap mergedImage = CombineBitmap(images, type);
+                mergedImage.Save(string.Format("{0}\\sprite.png", AppDomain.CurrentDomain.BaseDirectory));
+
+                MessageBox.Show("The sprite image and style have been generated successfully! :)");
+            }
+            else
+            {
+                MessageBox.Show("Please select the path");
+            }
+        }
+
+        private static Bitmap CombineBitmap(string[] files, SpriteType spriteType)
+        {
+            //read all images into memory
+            List<Bitmap> images = new List<Bitmap>();
+            Bitmap finalImage = null;
+
+            try
+            {
+                int width = 0;
+                int height = 0;
+                StringBuilder imageStyle = new StringBuilder();
+                StringBuilder iamgeContainer = new StringBuilder();
+
+                foreach (string image in files)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(image);
+                    string className = GetClassName(fileName);
+
+                    //create a Bitmap from the file and add it to the list
+                    Bitmap bitmap = new Bitmap(image);
+
+                    //update the size of the final bitmap
+                    if (spriteType == SpriteType.Horizontal)
+                    {
+                        string style = string.Format(IMAGE_STILE_H, className, bitmap.Width, bitmap.Height, (0 - width), Environment.NewLine);
+                        imageStyle.Append(style);
+
+                        if (className.IndexOf(":") == -1)
+                        {
+                            string container = string.Format(IMAGE_CONTAINER, fileName, Environment.NewLine);
+                            iamgeContainer.Append(container);
+                        }
+
+                        width += bitmap.Width;
+                        height = bitmap.Height > height ? bitmap.Height : height;
+                    }
+                    else
+                    {
+                        string style = string.Format(IMAGE_STILE_V, className, bitmap.Width, bitmap.Height, (0 - height), Environment.NewLine);
+                        imageStyle.Append(style);
+
+                        if (className.IndexOf(":") == -1)
+                        {
+                            string container = string.Format(IMAGE_CONTAINER, fileName, Environment.NewLine);
+                            iamgeContainer.Append(container);
+                        }
+
+                        height += bitmap.Height;
+                        width = bitmap.Width > width ? bitmap.Width : width;
+                    }
+
+                    images.Add(bitmap);
+                }
+
+                //create a bitmap to hold the combined image
+                finalImage = new Bitmap(width, height);
+
+                //get a graphics object from the image so we can draw on it
+                using (Graphics g = Graphics.FromImage(finalImage))
+                {
+                    //set background color
+                    g.Clear(Color.Transparent);
+
+                    //go through each image and draw it on the final image
+                    int offset = 0;
+                    foreach (Bitmap image in images)
+                    {
+                        if (spriteType == SpriteType.Horizontal)
+                        {
+                            g.DrawImage(image,
+                              new Rectangle(offset, 0, image.Width, image.Height));
+                            offset += image.Width;
+                        }
+                        else
+                        {
+                            g.DrawImage(image,
+                              new Rectangle(0, offset, image.Width, image.Height));
+                            offset += image.Height;
+                        }
+                    }
+                }
+
+                WriteFile(imageStyle.ToString(), iamgeContainer.ToString());
+
+                return finalImage;
+            }
+            catch (Exception ex)
+            {
+                if (finalImage != null)
+                    finalImage.Dispose();
+
+                throw ex;
+            }
+            finally
+            {
+                //clean up memory
+                foreach (Bitmap image in images)
+                {
+                    image.Dispose();
+                }
+            }
+        }
+
+        private static string GetClassName(string fileName)
+        {
+            string[] cascading = fileName.Split('.');
+            int length = cascading.Length;
+            StringBuilder className = new StringBuilder();
+
+            for (int index = 0; index < length; index++)
+            {
+                className.Append(" .");
+                string temp = cascading[index];
+                int indexConnector = temp.LastIndexOf('-');
+                int indexUnderline = temp.LastIndexOf('_');
+
+                if (indexConnector == -1 && indexUnderline == -1)
+                {
+                    // filename e.g: wechat
+                    // return        wechat
+                    //return temp;
+                    className.Append(temp);
+                    continue;
+                }
+
+                string name = ParseFileName(temp, indexConnector);
+                if (string.IsNullOrEmpty(name))
+                {
+                    //return ParseFileName(temp, indexUnderline);
+                    className.Append(ParseFileName(temp, indexUnderline));
+                    continue;
+                }
+                else
+                {
+                    //return name;
+                    className.Append(name);
+                }
+
+            }
+
+            return className.ToString().TrimStart(' ');
+        }
+
+        private static string ParseFileName(string name, int index)
+        {
+            string pseudo;
+            if (index > -1)
+            {
+                pseudo = name.Substring(index + 1);
+                if (pseudoClassList.Contains(pseudo))
+                {
+                    // filename e.g:  wechat-hover 
+                    //                wechat_hover  
+                    // return        .wechat:hover
+                    return string.Format("{0}:{1}", name.Substring(0, index), pseudo);
+                }
+
+                return name;
+            }
+            return string.Empty;
+        }
+
+        private static void WriteFile(string style, string container)
+        {
+            string filePath = string.Format("{0}\\sprite.html", AppDomain.CurrentDomain.BaseDirectory);
+
+            StreamWriter sw = new StreamWriter(filePath, false, Encoding.UTF8);
+            sw.Write(string.Format(HTML_TEMPLATE, style, container));
+            sw.Close();
+            sw.Dispose();
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = fbdDialog.ShowDialog();
+            if (dialogResult.ToString().ToLower() == "ok")
+            {
+                txtPath.Text = fbdDialog.SelectedPath;
+            }
+        }
+
+        private void llMail_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("Mailto:112055730@qq.com");
+        }
+
+        private void sprite_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            MessageBox.Show(@"Here are the rules of image naming:
+wechat => .wechat
+wechat_hover => .wechat:hover
+wechat-hover => .wechat:hover
+wechat_ibg_hover => .wechat_ibg:hover
+wechat-ibg-hover => .wechat-ibg:hover
+wechat.ibg => .wechat .ibg
+wechat.ibg.wechat_focus => .wechat .ibg .wechat:focus
+wechat.ibg.wechat_none => .wechat .ibg . wechat_none
+wechat_hover.wechat_none => .wechat:hover .wechat_none
+wechat-hover.wechat-none => .wechat:hover .wechat-none");
+        }
+    }
+}
